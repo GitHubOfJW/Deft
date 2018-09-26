fbe6ef5c14e6919e1c6295b79eb07f2f56a97618
zhujianwei <zhujianwei_ext@smartisan.com>
2018-09-26 15:21:51 +0800

    feat: 列表查询

--- DeftProject/src/controllers/adminManager/AdminController.js
+++ DeftProject/src/controllers/adminManager/AdminController.js
@@ -15,33 +15,42 @@ class AdminController extends BaseController {
 
   //管理管理员列表请求
   async adminList(req,res){
-    const page = req.body.page | 1;
-    const pageSize = req.body.pageSize | 1;
+    const start = req.body.start;
+    const end = req.body.end;
+    const username = req.body.username;
+    const page = req.body.nextpage | 1;
+    const prePage =  req.body.page | 1;
+    const pageSize = req.body.pageSize | 8;
     
     const count = await adminModel.totalCount();
+
+    console.log(page);
 
     // 计算页数
     const totalPage = (count +  pageSize - 1) / pageSize;
 
     // 遍历
     let pagination =  [];
-    const start = page - 2 > 0 ? page - 2 : 1;
-    const end = page + 2 > totalPage ? totalPage : page + 2;
-    for(let i = start; i <= end;i++){
-      pagination.push({
-        page:i
-      })
-    }
+
+    let startPage = page - 2 > 0 ? page - 2 : 1;
+    let endPage =  startPage + 4 > totalPage ? totalPage : startPage + 4;
     
-    const list = await adminModel.list(page,pageSize)
+    const conditions = {
+      page:page,
+      start:start,
+      end:end,
+      username:username
+    };
+    const list = await adminModel.list(page,pageSize,conditions)
 
     const data =  {
       list:list,
       totalCount:count,
-      pagination:pagination,
-      conditions:{
-        page:page
-      }
+      pagination:{
+        start:startPage,
+        end:endPage
+      },
+      conditions:conditions
     }
     const result = super.handlerResponseData(1,data,'获取成功')
     res.json(result);
